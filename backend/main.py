c05d88ad0a8ea69d92e3bed0dfd8377b5c709984
Pedro Costa <pedrocosta99@gmail.com>
2025-07-30 22:32:56 +0800

    feat: venv + main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import upload, quiz
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(upload.router)
+app.include_router(quiz.router)
