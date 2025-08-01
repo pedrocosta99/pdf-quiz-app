fb8f80b7ab2ef32fbc7c98278a59bbbac0772b59
Pedro Costa <pedrocosta99@gmail.com>
2025-08-01 19:33:45 +0800

    feat: removes mocks

--- quiz-app-frontend/src/hooks/useGenerateQuiz.ts
+++ quiz-app-frontend/src/hooks/useGenerateQuiz.ts
@@ -1,13 +1,10 @@
 // hooks/useGenerateQuiz.ts
 import { useMutation } from '@tanstack/react-query';
 import { Question } from '@/types';
-import { mockQuestions } from '@/mock';
 
 export function useGenerateQuiz() {
   return useMutation({
     mutationFn: async ({ text, questionCount }: { text: string; questionCount: number }): Promise<Question[]> => {
-    // await new Promise((resolve) => setTimeout(resolve, 500)); // Simulate a delay
-    // return mockQuestions; // For testing purposes, return mock questions
     const res = await fetch(`http://localhost:8000/generate-quiz`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
