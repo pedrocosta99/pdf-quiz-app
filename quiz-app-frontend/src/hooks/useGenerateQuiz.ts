97d08934a821f81afd324afd86b1e7a51a8d73f5
Pedro Costa <pedrocosta99@gmail.com>
2025-08-01 11:34:35 +0800

    test: unmock questions

--- quiz-app-frontend/src/hooks/useGenerateQuiz.ts
+++ quiz-app-frontend/src/hooks/useGenerateQuiz.ts
@@ -6,8 +6,8 @@ import { mockQuestions } from '@/mock';
 export function useGenerateQuiz() {
   return useMutation({
     mutationFn: async ({ text, questionCount }: { text: string; questionCount: number }): Promise<Question[]> => {
-    await new Promise((resolve) => setTimeout(resolve, 500)); // Simulate a delay
-    return mockQuestions; // For testing purposes, return mock questions
+    // await new Promise((resolve) => setTimeout(resolve, 500)); // Simulate a delay
+    // return mockQuestions; // For testing purposes, return mock questions
     const res = await fetch(`http://localhost:8000/generate-quiz`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
